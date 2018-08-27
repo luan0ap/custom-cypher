e452614e91b24895879424ed478c40d3d852be46
luna0ap <luanap.kun@gmail.com>
2018-08-27 14:10:41 -0300

    created method to show errors, added webpack and babel

---
+++ src/custom-cypher.js
@@ -0,0 +1,36 @@
+class Cipher {
+  constructor(hash = {}) {
+    this.hasJustALetter(hash) ? this.hash = hash : this.syntaxError('Just one letter in object properties are allowed')
+  }
+
+  hasJustALetter(o) {
+    return !Object.values(o).filter(s => s.length > 1).length
+  }
+
+  syntaxError (str) {
+    throw new SyntaxError(str)
+  }
+
+  getLetter (str = '', obj = {}) {
+    return str.split('').map(w => obj[w] || w).join('')
+  }
+
+  encrypt(str) {
+    return this.getLetter(str, this.hash)
+  }
+
+  decrypt(hash) {
+    const swap = (obj) => {
+      let o = {}
+      for (const p in obj) {
+        o[obj[p]] = p
+      }
+      return o
+    }
+
+    const sHash = swap(this.hash)
+    return this.getLetter(hash, sHash)
+  }
+}
+
+module.export = Cipher
